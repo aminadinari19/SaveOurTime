c3f5feb2f0e9b78911edb201d88b366f6a155754
Odbayar Erdenebat <61918171+odiisus@users.noreply.github.com>
2021-10-07 15:09:35 +0200

    Basic homepage without the chatbot

--- source_files/frontend/src/app/app.module.ts
+++ source_files/frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { FAQComponent } from './faq/faq.component';
+import { AppointComponent } from './appoint/appoint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutpageComponent,
+    FAQComponent,
+    AppointComponent
   ],
   imports: [
     BrowserModule,
---
+++ source_files/frontend/src/app/appoint/appoint.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-appoint',
+  templateUrl: './appoint.component.html',
+  styleUrls: ['./appoint.component.css']
+})
+export class AppointComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
