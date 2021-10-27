07c8c2237503c33309ae2df701896d163d2b8b5a
Odbayar Erdenebat <61918171+odiisus@users.noreply.github.com>
2021-10-27 21:03:41 +0200

    Homepage with the routing
    Set the routing so that every component is linked

--- source_files/frontend/src/app/app-routing.module.ts
+++ source_files/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { AppointComponent } from './appoint/appoint.component';
+import { FAQComponent } from './faq/faq.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about', component: AboutpageComponent},
+  {path:'faq', component: FAQComponent},
+  {path:'appoint', component: AppointComponent},
+  {path:'', component: HomepageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- source_files/frontend/src/app/app.module.ts
+++ source_files/frontend/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutpageComponent } from './aboutpage/aboutpage.component';
 import { FAQComponent } from './faq/faq.component';
 import { AppointComponent } from './appoint/appoint.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+const routes: Routes = [
+  {path:'about', component: AboutpageComponent},
+  {path:'faq', component: FAQComponent},
+  {path:'appoint', component: AppointComponent},
+  //{path:'', component:HomepageComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutpageComponent,
     FAQComponent,
-    AppointComponent
+    AppointComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
