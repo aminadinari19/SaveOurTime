3b0cbfaab3d48dffb135475a1489b19e1032a5b4
aminadinari19 <71690455+aminadinari19@users.noreply.github.com>
2021-11-12 13:13:56 +0100

    Add compodoc tool
    * Add compodoc
    
    * Added types to tsconfig.app.json
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Delete .DS_Store
    
    * Update ci.yml
    
    * Update package-lock.json
    
    * Added types
    
    * Added types to make the website work
    
    * Update ci.yml
    
    * Update ci.yml
    
    Co-authored-by: Amina Manzoor Qadir Dinari <qjvad4@inf.elte.hu>

--- source_files/frontend/src/app/aboutpage/aboutpage.component.ts
+++ source_files/frontend/src/app/aboutpage/aboutpage.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './aboutpage.component.html',
   styleUrls: ['./aboutpage.component.css']
 })
+/** Hello How are you */
 export class AboutpageComponent implements OnInit {
   ngOnInit(): void {
     null;
