2503a04b52aefcafdad1b24a7ae0dbf1472cff44
mastertimisensei <timilehinbisolaojo@gmail.com>
2021-11-18 19:45:40 +0100

    The Appointment Page:  It uses Calendly API which is linked to a Google Calender  This page handles the Appointment Booking

--- source_files/frontend/src/app/appoint/appoint.component.ts
+++ source_files/frontend/src/app/appoint/appoint.component.ts
@@ -1,12 +1,16 @@
 import {Component, OnInit} from '@angular/core';
-
+export {}; declare global { interface Window { Calendly: any; } } 
 @Component({
   selector: 'app-appoint',
   templateUrl: './appoint.component.html',
   styleUrls: ['./appoint.component.css'],
 })
+
 export class AppointComponent implements OnInit {
   ngOnInit(): void {
-    null;
+    window.Calendly.initInlineWidget({
+      url: 'https://calendly.com/timilehinbisolaojo/appointment-1',
+      parentElement: document.querySelector('.calendly-inline-widget'),
+    });
   }
 }
