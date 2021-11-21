a6575760a979a9c074ac4be534824a329f3e63af
Amina Manzoor Qadir Dinari <qjvad4@inf.elte.hu>
2021-11-21 20:51:12 +0100

    Update server.js

--- source_files/frontend/server.js
+++ source_files/frontend/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/sot'));
+app.use(express.static('./dist/SOT'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('dist/sot/index.html', {root: 'dist/sot/'}),
+    res.sendFile('dist/src/index.html', {root: 'dist/SOT'}),
 );
 
 // Start the app by listening on the default Heroku port
