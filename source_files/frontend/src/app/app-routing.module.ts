efe69001d1fb534d91ad1d627d0d4f4f2ce6db9e
Amina Manzoor Qadir Dinari <qjvad4@inf.elte.hu>
2021-11-29 20:22:35 +0100

    Update app-routing.module.ts

--- source_files/frontend/src/app/app-routing.module.ts
+++ source_files/frontend/src/app/app-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
   {path: '', component: HomepageComponent}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
