e73ac12866d630a60a20586b1773c0146568e115
aminadinari19 <71690455+aminadinari19@users.noreply.github.com>
2021-12-10 18:29:17 +0100

    Added feedback form
    * Added feedback form
    
    * Update report.component.html
    
    * Update release.yml
    
    Co-authored-by: Amina Manzoor Qadir Dinari <qjvad4@inf.elte.hu>

--- source_files/frontend/src/app/app-routing.module.ts
+++ source_files/frontend/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import {AboutpageComponent} from './aboutpage/aboutpage.component';
 import {AppointComponent} from './appoint/appoint.component';
 import {FAQComponent} from './faq/faq.component';
 import {HomepageComponent} from './homepage/homepage.component';
+import {ReportComponent } from './report/report.component';
 
 const routes: Routes = [
   {path: 'about', component: AboutpageComponent},
   {path: 'faq', component: FAQComponent},
   {path: 'appoint', component: AppointComponent},
-  {path: '', component: HomepageComponent}];
+  {path: '', component: HomepageComponent},
+  {path: 'report', component: ReportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
